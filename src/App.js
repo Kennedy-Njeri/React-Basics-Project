1c906f65e96b20bae96edbe85b07de231aca68e0
Kennedy Njeri <mistakenz123@gmail.com>
2019-09-11 16:49:25 +0300

    React setup, working with components and props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
+import Contact from "./components/Contact";
+import Header from "./components/Header";
+
 import './App.css';
 
 function App() {
+
+  // const name = "Kennedy"
+  // const hello = true
+  // const showMath = false
+  //
+  // let math
+  //
+  // if(!showMath) {
+  //   math = <h4>1+1 = {1+1}</h4>
+  // } else {
+  //   math = null
+  // }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header branding="Contact Manager"/>
+      <Contact name="Kennedy" email="kennedy@gmail.com" phone="0704107711" />
+      <Contact name="Carol" email="Carol@gmail.com" phone="0704107814" />
+      {/*{hello ? <h2>Hello: {name.toUpperCase() }</h2> : null }*/}
+
+      {/*{math}*/}
+
     </div>
   );
 }
