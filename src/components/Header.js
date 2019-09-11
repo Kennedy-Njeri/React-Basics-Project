57bff476d30de47697bb700f46a9c7eaf0176374
Kennedy Njeri <mistakenz123@gmail.com>
2019-09-11 19:47:35 +0300

    default props and props validation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,31 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+
+
 
 const Header = (props) => {
     const {branding} = props
     return (
         <div>
-            <h1>{branding}</h1>
+            <h1 style={{color:'red', fontSize: '50px'}}>{branding}</h1>
         </div>
     )
 }
+
+
+
+
+
+// validation of the data received
+Header.propTypes = {
+    branding: PropTypes.string.isRequired
+}
+
+// default prop types...will display even if there is nothing in the header
+Header.defaultProps = {
+    branding: "My App"
+}
+
 
 
 export default Header
