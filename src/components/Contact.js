ade14c48c2356c00cfce6ffff97c42a90e72828b
Kennedy Njeri <mistakenz123@gmail.com>
2019-09-12 20:18:00 +0300

    event handlers

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,11 +5,17 @@ import PropTypes from 'prop-types'
 
 
 class Contact extends Component {
-    state = {}
-
-    onShowClick = (name, e) => {
-        console.log(name)
+    state = {
+        showContactInfo: false
     }
+    onDeleteClick= () => {
+        //console.log("clicked")
+        this.props.deleteClickHandler()
+    }
+
+    // onShowClick = (e) => {
+    //     this.setState({showContactInfo: !this.state.showContactInfo})
+    // }
 
     // static propTypes = {
     //     name: PropTypes.string.isRequired,
@@ -19,13 +25,20 @@ class Contact extends Component {
 
     render() {
         const {name, email, phone } = this.props.contact
+        const { showContactInfo } = this.state
         return (
             <div className="card card-body mb-3">
-                <h4>{name} <i onClick={this.onShowClick} className="fas fa-sort-down"></i> </h4>
-                <ul className="list-group">
+                <h4>{name} <i onClick={() =>
+                    this.setState({showContactInfo: !this.state.showContactInfo}) } className="fas fa-sort-down" style={{cursor: 'pointer'}}></i>
+
+                    <i className="fas fa-times" style={{cursor: 'pointer', float:'right', color:'red'}} onClick={this.onDeleteClick} />
+
+                </h4>
+
+                {showContactInfo ? ( <ul className="list-group">
                     <li className="list-group-item">Email: {email}</li>
                     <li className="list-group-item">Phone: {phone}</li>
-                </ul>
+                </ul>) : null}
             </div>
         );
     }
@@ -35,6 +48,7 @@ class Contact extends Component {
 
 Contact.propTypes = {
     contact: PropTypes.object.isRequired,
+    deleteClickHandler: PropTypes.func.isRequired
     // email:PropTypes.string.isRequired,
     // phone:PropTypes.string.isRequired,
 }
