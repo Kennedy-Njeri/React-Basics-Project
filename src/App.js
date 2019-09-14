ab0ddef750bda92e0b0dfbcc24e9444050935a85
Kennedy Njeri <mistakenz123@gmail.com>
2019-09-14 14:31:42 +0300

    forms

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/layout/Header";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import {Provider} from "./context";
 import './App.css';
+import AddContact from "./components/contacts/AddContact";
 
 
 function App() {
@@ -26,6 +27,7 @@ function App() {
     <div className="App">
       <Header branding="Contact Manager"/>
         <div className="container">
+            <AddContact />
            <Contacts />
         </div>
     </div>
