ea32172f423bee504a02e6a41c3dae0c37d61818
Kennedy Njeri <mistakenz123@gmail.com>
2019-09-14 13:00:24 +0300

    directory fix

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import Contacts from "./components/Contacts";
-import Header from "./components/Header";
+import Contacts from "./components/contacts/Contacts";
+import Header from "./components/layout/Header";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import {Provider} from "./context";
 import './App.css';
+
 
 function App() {
 
